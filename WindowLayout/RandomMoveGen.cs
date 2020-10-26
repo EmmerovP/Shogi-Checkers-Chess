5bd21c6b652debe8177e3b231e671fdc26c96f1c
petra.emmerova <petra.emmerova@gmail.com>
2020-10-26 19:06:06 +0100

    AI works pretty well. Going to implement alpha-beta.

--- WindowLayout/RandomMoveGen.cs
+++ WindowLayout/RandomMoveGen.cs
@@ -140,7 +140,7 @@ namespace ShogiCheckersChess
             }
 
             //Vygenerujeme možné tahy na momentální šachovnici
-            for (int i = 0; i < Board.board.GetLength(0); i++)
+            for (int i = 0; i < Board.board.GetLength(0); i++)  
             {
                 for (int j = 0; j < Board.board.GetLength(0); j++)
                 {
